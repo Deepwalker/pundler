3799269b2f1722d3090dc18912fcec09f0aaa9ae
Mikhail Krivushin <krivushinme@gmail.com>
2015-02-01 19:06:03 +0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os.path
+
+
+def read(fname):
+    try:
+        return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        return ''
+
+
+setupconf = dict(
+    name='pundle',
+    version='0.1.1',
+    license='BSD',
+    url='https://github.com/Deepwalker/pundler/',
+    author='Deepwalker',
+    author_email='krivushinme@gmail.com',
+    description=('Requirements management tool.'),
+    long_description=read('README.md'),
+    keywords='bundler virtualenv pip install package setuptools',
+
+    py_modules=['pundle'],
+    entry_points=dict(
+        console_scripts=[
+            'pundle = pundle:main'
+        ]
+    ),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
+
+if __name__ == '__main__':
+    setup(**setupconf)
