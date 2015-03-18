c73485c1314117302a6f40e9245305916e2f401a
Mikhail Krivushin <krivushinme@gmail.com>
2015-03-18 15:06:23 +0400

    0.2.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setupconf = dict(
     name='pundle',
-    version='0.2.2',
+    version='0.2.3',
     license='BSD',
     url='https://github.com/Deepwalker/pundler/',
     author='Deepwalker',
